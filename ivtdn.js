0d6dd0231a32ca6d915f2a7798c2f1f808ae21c9
Vlad Rutskiy <vladrytskiy@gmail.com>
2023-11-17 19:19:39 +0200

    started a new page

---
+++ ivtdn.js
@@ -0,0 +1,7 @@
+// first lesson = intro
+/*
+let my = document.querySelector("#myDiv");
+my.addEventListener("click", function(){
+    my.innerHTML = "have a good day";   // need to find out how to refer to html file here my.innerHTML is incorrect reference.
+})
+*/
