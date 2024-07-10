1d272df7d80b63f89488e0e567a6b7d7aadf8daa
Vlad Rutskiy <vladrytskiy@gmail.com>
2024-07-10 14:11:07 -0600

    Added dynamic date

---
+++ year-footer.js
@@ -0,0 +1,5 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Set the text content of the element with id 'copyright-year' to the current year
+document.getElementById('copyright-year').textContent = currentYear;
