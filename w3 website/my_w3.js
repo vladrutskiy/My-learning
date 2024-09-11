e54ca1abbb0b6e0d68d2476169fa236ca036c38f
Vlad Rutskiy <vladrytskiy@gmail.com>
2024-09-10 19:44:58 -0600

    1. Added SeeHearAct project 2. Fixed "upward" and "see" headers size

--- w3 website/my_w3.js
+++ w3 website/my_w3.js
@@ -43,3 +43,35 @@ function myFunction1() {
         moreText.style.display = "inline";
     }
 }
+
+function upwardFunction() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn2");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";  // Show dots if content is collapsed
+        btnText.innerHTML = "Load more";  // Change button text
+        moreText.style.display = "none";  // Hide additional content
+    } else {
+        dots.style.display = "none";  // Hide dots if content is expanded
+        btnText.innerHTML = "Load less";  // Change button text
+        moreText.style.display = "inline";  // Show additional content
+    }
+}
+
+function seeFunction() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("myBtn3");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";  // Show dots if content is collapsed
+        btnText.innerHTML = "Load more";  // Change button text
+        moreText.style.display = "none";  // Hide additional content
+    } else {
+        dots.style.display = "none";  // Hide dots if content is expanded
+        btnText.innerHTML = "Load less";  // Change button text
+        moreText.style.display = "inline";  // Show additional content
+    }
+}
