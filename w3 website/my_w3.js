e0d0ba7e84415539e1d238ba2c693ff973da4d27
Vlad Rutskiy <vladrytskiy@gmail.com>
2024-09-23 19:04:36 -0600

    added contact info

--- w3 website/my_w3.js
+++ w3 website/my_w3.js
@@ -27,120 +27,7 @@ function toggleFunction() {
     }
 }
 
-//load more button functionality
-function myFunction1() {
-    var dots = document.getElementById("dots1");
-    var moreText = document.getElementById("more1");
-    var btnText = document.getElementById("myBtn1");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Load more";
-        moreText.style.display = "none";
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "Load less";
-        moreText.style.display = "inline";
-    }
-}
-
-function upwardFunction() {
-    var dots = document.getElementById("dots2");
-    var moreText = document.getElementById("more2");
-    var btnText = document.getElementById("myBtn2");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";  // Show dots if content is collapsed
-        btnText.innerHTML = "Load more";  // Change button text
-        moreText.style.display = "none";  // Hide additional content
-    } else {
-        dots.style.display = "none";  // Hide dots if content is expanded
-        btnText.innerHTML = "Load less";  // Change button text
-        moreText.style.display = "inline";  // Show additional content
-    }
-}
-
-function seeFunction() {
-    var dots = document.getElementById("dots3");
-    var moreText = document.getElementById("more3");
-    var btnText = document.getElementById("myBtn3");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";  // Show dots if content is collapsed
-        btnText.innerHTML = "Load more";  // Change button text
-        moreText.style.display = "none";  // Hide additional content
-    } else {
-        dots.style.display = "none";  // Hide dots if content is expanded
-        btnText.innerHTML = "Load less";  // Change button text
-        moreText.style.display = "inline";  // Show additional content
-    }
-}
-
-function abbeydaleFunction() {
-    var dots = document.getElementById("dots4");
-    var moreText = document.getElementById("more4");
-    var btnText = document.getElementById("myBtn4");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";  // Show dots if content is collapsed
-        btnText.innerHTML = "Load more";  // Change button text
-        moreText.style.display = "none";  // Hide additional content
-    } else {
-        dots.style.display = "none";  // Hide dots if content is expanded
-        btnText.innerHTML = "Load less";  // Change button text
-        moreText.style.display = "inline";  // Show additional content
-    }
-}
-
-function portsideFunction() {
-    var dots = document.getElementById("dots5");
-    var moreText = document.getElementById("more5");
-    var btnText = document.getElementById("myBtn5");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";  // Show dots if content is collapsed
-        btnText.innerHTML = "Load more";  // Change button text
-        moreText.style.display = "none";  // Hide additional content
-    } else {
-        dots.style.display = "none";  // Hide dots if content is expanded
-        btnText.innerHTML = "Load less";  // Change button text
-        moreText.style.display = "inline";  // Show additional content
-    }
-}
-
-function reflektFunction() {
-    var dots = document.getElementById("dots6");
-    var moreText = document.getElementById("more6");
-    var btnText = document.getElementById("myBtn6");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";  // Show dots if content is collapsed
-        btnText.innerHTML = "Load more";  // Change button text
-        moreText.style.display = "none";  // Hide additional content
-    } else {
-        dots.style.display = "none";  // Hide dots if content is expanded
-        btnText.innerHTML = "Load less";  // Change button text
-        moreText.style.display = "inline";  // Show additional content
-    }
-}
-
-function projectsFunction() {
-    var dots = document.getElementById("dots7");
-    var moreText = document.getElementById("more7");
-    var btnText = document.getElementById("myBtn7");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";  // Show dots if content is collapsed
-        btnText.innerHTML = "Load more";  // Change button text
-        moreText.style.display = "none";  // Hide additional content
-    } else {
-        dots.style.display = "none";  // Hide dots if content is expanded
-        btnText.innerHTML = "Load less";  // Change button text
-        moreText.style.display = "inline";  // Show additional content
-    }
-}
-
-
+// typewriter function
 function typeWriter() {
     if (i < txt.length) {
       document.getElementById("demo").innerHTML += txt.charAt(i);
@@ -164,3 +51,19 @@ function typeWriter() {
     }
   }
 
+//hide and show content (load more button functionality)
+function toggleContent(dotsId, moreTextId, btnId) {
+    var dots = document.getElementById(dotsId);
+    var moreText = document.getElementById(moreTextId);
+    var btnText = document.getElementById(btnId);
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";  // Show dots if content is collapsed
+        btnText.innerHTML = "Load more";  // Change button text
+        moreText.style.display = "none";  // Hide additional content
+    } else {
+        dots.style.display = "none";  // Hide dots if content is expanded
+        btnText.innerHTML = "Load less";  // Change button text
+        moreText.style.display = "inline";  // Show additional content
+    }
+}
